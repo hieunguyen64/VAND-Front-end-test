176e279e6afa99a1a072d67085b4ed374822c4cd
Hieu Nguyen <hieu.nd@fahasa.com.vn>
2022-12-01 16:43:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+How to run
+Clone the project
+Go to the folder application using cd command on your cmd or terminal
+Run npm install on your cmd or terminal
+Run npm start on your cmd or terminal
+Open browser and paste the address
---
+++ src/Components/Covid/Detail/index.js
@@ -0,0 +1,72 @@
+import { Box, Dialog, DialogTitle, Alert, Snackbar, Typography } from '@mui/material'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+
+export default function DetailCountry(props) {
+  const { CountryCode, open, onClose } = props;
+  const [error, setError] = React.useState("");
+  const [isError, setIsError] = React.useState(false);
+  const [data, setData] = React.useState({})
+  console.log('GO')
+  const apiGetDetail = async () => {
+    axios.get(`https://restcountries.com/v2/alpha/${CountryCode}`)
+      .then(function (response) {
+        console.log(response)
+        setData(response.data);
+      })
+      .catch(function (error) {
+        setError(error)
+        handleOpenToastError()
+      });
+  }
+
+  const handleOpenToastError = () => {
+    setIsError(true);
+  };
+
+  const handleCloseToastError = () => {
+    setIsError(false);
+  };
+
+  useEffect(() => {
+    apiGetDetail()
+  }, [CountryCode])
+  return (
+    data?.flag && (
+      <Dialog open={open} onClose={onClose} >
+        <DialogTitle sx={{ fontWeight: 800, fontSize: 25 }}>Thông tin nước {data?.name}: </DialogTitle>
+
+        <Box sx={{ width: 500, padding: 4, display: 'flex', justifyContent: "center", alignItems: "center", flexDirection: "row" }}>
+          <img src={data?.flag} style={{ width: '200px', height: '150px' }} alt={data?.name} />
+          <Box sx={{ margin: '0 16px' }}>
+            <Typography>
+              <strong>Tên nước: </strong>{data?.name}
+            </Typography>
+            <Typography>
+              <strong>Dân số: </strong> {data?.population.toLocaleString('vi-VN')} người
+            </Typography>
+            <Typography>
+              <strong>Thủ đô: </strong> {data?.capital}
+            </Typography>
+            <Typography>
+              <strong>Khu vực: </strong> {data?.region}
+            </Typography>
+            <Typography>
+              <strong>Khu vực phụ: </strong> {data?.subregion}
+            </Typography>
+          </Box>
+        </Box>
+
+        <Snackbar anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }} open={isError} autoHideDuration={3000} onClose={handleCloseToastError}>
+          <Alert severity="error" onClose={handleCloseToastError} sx={{ width: '100%' }}>
+            {error}
+          </Alert>
+        </Snackbar>
+
+      </Dialog>
+    )
+  )
+}
